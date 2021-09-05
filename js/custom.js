44f009dc6e88cee3a112c341994f0c9e4262a551
Debraj Sikdar <debrajsikdar.personal@gmail.com>
2021-09-05 21:29:55 +0530

    Complete

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        780:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+//  NAVIGATION TOGGLE //
+
+  $('.toggle-btn').click(function(){
+    $('header nav').fadeIn(800);
+  });
+  $('.nav-close').click(function(){
+    $('header nav').fadeOut(1000);
+  });
+
+//  NAVIGATION TOGGLE //
+
+  $.stellar();
+
+});
